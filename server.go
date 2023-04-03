b8dccbe9536c265e10716d6c5da0f81de520342f
Michael Okoh <ordrizzy@gmail.com>
2023-04-03 01:08:34 +0100

    fix existing values

--- server.go
+++ server.go
@@ -22,24 +22,70 @@ type Data struct {
 func main() {
 	router := chi.NewRouter()
 
+	_ = godotenv.Load()
+
+	redisHost, exist := os.LookupEnv("REDIS_HOST")
+
+	if !exist {
+		log.Fatal("REDIS_HOST not set in .env")
+	}
+
+	redisPort, exist := os.LookupEnv("REDIS_PORT")
+
+	if !exist {
+		log.Fatal("REDIS_PORT not set in .env")
+	}
+
+	redisPass, exist := os.LookupEnv("REDIS_PASS")
+
+	if !exist {
+		log.Fatal("REDIS_PASS not set in .env")
+	}
+
+	redisScheme, exist := os.LookupEnv("REDIS_SCHEME")
+
+	if !exist {
+		log.Fatal("REDIS_SCHEME not set in .env")
+	}
+
+	if err := database.ConnectRedis(redisHost, redisPort, redisPass, redisScheme); err != nil {
+		log.Fatal(err)
+	}
+
+	latency, _ := database.Redis.Get("latency_the_saver").Result()
+
+	var savedAmounts []int
+
+	_ = json.Unmarshal([]byte(latency), &savedAmounts)
+
 	router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
-		// Initialize the random number generator
-		rand.Seed(time.Now().UnixNano())
+		var amount int
 
-		// Generate a random integer between 1 and 100
-		amount := rand.Intn(100) + 1
+		var message string
 
-		messages := []string{
-			"if you've saved this amount before better refresh",
-			"garri incomming",
-			"better save this",
-			"don't cheat",
-			"save this one for latency",
-			"sugar mummy",
+		for {
+			// Initialize the random number generator
+			rand.Seed(time.Now().UnixNano())
+
+			// Generate a random integer between 1 and 100
+			amount = rand.Intn(100) + 1
+
+			messages := []string{
+				"if you've saved this amount before better refresh",
+				"garri incomming",
+				"better save this",
+				"don't cheat",
+				"save this one for latency",
+				"sugar mummy",
+			}
+
+			randIndex := rand.Intn(len(messages))
+			message = messages[randIndex]
+
+			if contains(savedAmounts, amount) == false {
+				break
+			}
 		}
-
-		randIndex := rand.Intn(len(messages))
-		message := messages[randIndex]
 
 		tmpl := template.Must(template.ParseFiles("index.html"))
 
@@ -49,42 +95,9 @@ func main() {
 		}
 
 		_ = tmpl.Execute(writer, data)
-
 	})
 
 	router.Get("/save", func(writer http.ResponseWriter, request *http.Request) {
-		_ = godotenv.Load()
-
-		redisHost, exist := os.LookupEnv("REDIS_HOST")
-
-		if !exist {
-			log.Fatal("REDIS_HOST not set in .env")
-		}
-
-		redisPort, exist := os.LookupEnv("REDIS_PORT")
-
-		if !exist {
-			log.Fatal("REDIS_PORT not set in .env")
-		}
-
-		redisPass, exist := os.LookupEnv("REDIS_PASS")
-
-		if !exist {
-			log.Fatal("REDIS_PASS not set in .env")
-		}
-
-		redisScheme, exist := os.LookupEnv("REDIS_SCHEME")
-
-		if !exist {
-			log.Fatal("REDIS_SCHEME not set in .env")
-		}
-
-		if err := database.ConnectRedis(redisHost, redisPort, redisPass, redisScheme); err != nil {
-			log.Fatal(err)
-		}
-
-		latency, _ := database.Redis.Get("latency_the_saver").Result()
-
 		amount, _ := strconv.Atoi(request.FormValue("amount"))
 
 		if latency == "" {
@@ -94,13 +107,9 @@ func main() {
 
 			database.Redis.Set("latency_the_saver", string(jsonData), 0)
 		} else {
-			var array []int
+			savedAmounts = append(savedAmounts, amount)
 
-			_ = json.Unmarshal([]byte(latency), &array)
-
-			array = append(array, amount)
-
-			jsonData, _ := json.Marshal(array)
+			jsonData, _ := json.Marshal(savedAmounts)
 
 			database.Redis.Set("latency_the_saver", string(jsonData), 0)
 		}
@@ -111,4 +120,17 @@ func main() {
 	if err := http.ListenAndServe(":7777", router); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func contains(arr []int, searchValue int) bool {
+	found := false
+
+	for _, value := range arr {
+		if value == searchValue {
+			found = true
+			break
+		}
+	}
+
+	return found
 }
